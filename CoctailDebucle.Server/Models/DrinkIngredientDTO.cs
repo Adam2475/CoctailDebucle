7685e667a2141de225f99527c0356bee10bf0025
Adam2475 <adamnatanpassaretti@gmail.com>
2025-05-01 13:57:50 +0200

    fixed dto required Name field, minor UI changes

--- CoctailDebucle.Server/Models/DrinkIngredientDTO.cs
+++ CoctailDebucle.Server/Models/DrinkIngredientDTO.cs
@@ -3,7 +3,7 @@
     public class DrinkIngredientDto
     {
         public int IngredientId { get; set; }
-        public string IngredientName { get; set; }  // ✅ Add this
+        public string? IngredientName { get; set; }  // ✅ Add this
         public string Amount { get; set; }
     }
 }
