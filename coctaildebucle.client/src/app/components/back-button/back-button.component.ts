029a22337821e9d1211dac050583645bbf233c1a
Adam2475 <adamnatanpassaretti@gmail.com>
2025-05-04 14:29:16 +0200

    admin page UI style finished

--- coctaildebucle.client/src/app/components/back-button/back-button.component.ts
+++ coctaildebucle.client/src/app/components/back-button/back-button.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { Location } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
+// Ng Prime UI
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-back-button',
-  imports: [TranslateModule],
+  imports: [TranslateModule, ButtonModule],
   templateUrl: './back-button.component.html',
   styleUrl: './back-button.component.css'
 })
